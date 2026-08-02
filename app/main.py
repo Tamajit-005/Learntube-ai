cd51e5cda38b4a9478e3ed55b775edd835faaab5
Tamajit-005 <tamajitsaha05@gmail.com>
2026-08-02 22:59:47 +0530

    Update url

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://learntube-ai.onrender.com"],
+    allow_origins=["http://localhost:3000", "https://learntube-6x70sja0w-tamajitsaha05-6876s-projects.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
