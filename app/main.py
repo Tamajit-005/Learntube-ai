07fa7fd4ad696d320cac2432bb582972c210af3f
Sumon Sen <sumon.animator@gmail.com>
2026-08-02 21:54:19 +0530

    Updated port

--- app/main.py
+++ app/main.py
@@ -23,4 +23,4 @@ app.include_router(router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=80, log_level="info")
